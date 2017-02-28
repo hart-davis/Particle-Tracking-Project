4a0b99cc9b9f3dd7799e6bf510c392607cc36b0b
hart-davis <mhartd@gmail.com>
2017-02-28 09:59:49 +0200

    Add files via upload

--- TheHDCode.py
+++ TheHDCode.py
@@ -1,13 +1,9 @@
 import tracpy
 import tracpy.plotting
-#import mike_plotting
 from tracpy.tracpy_class import Tracpy
 import datetime
 import matplotlib
 from tracpy import op
-#import Mike_Test_Case
-#import tracpy_modified
-#from tracpy import inout
 import xray
 from numpy import where, squeeze, array, sqrt, zeros
 import os
@@ -18,13 +14,13 @@ from numpy import empty, size, unique, diff, asfortranarray
 
 matplotlib.rcParams.update({'font.size': 20})
 # this is the location of a high resolution model TXLA (texas something) Change in all files
-loc = '/home/hart-davis/Desktop/Mike_PTA/Globcurrent_data_final.nc'
+loc = '/home/hart-davis/Desktop/Mike_PTA/Globcurrent_March2008.nc'
 # number of days that this model will be run. This example ranges from 1 to 24
 ndays = 1
 # this is the starting date in a datetime
 date = datetime.datetime (2008, 3, 1, 0, 0, 0)
 # this the time between outputs in the correct format (seconds)
-tseas = 3*3600
+tseas = 4*3600
 
 time_units = 'seconds since 1970-01-01'
 # sets a smaller limit than between model outputs for when to force interpolation if it hasn't already occured
@@ -49,8 +45,8 @@ num_layers = 1
 
 zpar = 0
 
-# nwgom is a built i projection that is used. Also - galveston and nwgom-pyproj
-proj = tracpy.tools.make_proj('madagascar')
+# we have cape_agulhas, durbs, SE_Atlantic East_Africa, east_equator and madagascar
+proj = tracpy.tools.make_proj('cape_agulhas')
 # read in the grid
 grid = tracpy.inout.readgrid(loc, proj,  usespherical = False)
 
@@ -66,8 +62,8 @@ grid.km = 1
 
 # initialization of the tracpy class
 tp = Tracpy (loc, grid, name=name, tseas=tseas, ndays=ndays, nsteps=nsteps, N=N, ff=ff, ah=ah, av=av, doturb=doturb, do3d=do3d, z0=z0, zpar=zpar, time_units=time_units)
-# input starting locations in the long and latitude
-lon0, lat0 = np.meshgrid(np.linspace(43.125,47.5,30), np.linspace(-24.125,-25.5,30))
+# input starting locations in the long and latitude -27.860   33.245
+lon0, lat0 = np.meshgrid(np.linspace(29.1,31.1,10), np.linspace(-31,-32,10))
 # removes points not found within the domain
 lon0, lat0 = tracpy.tools.check_points(lon0, lat0, tp.grid)
 # timing that the grid was already read in
@@ -75,4 +71,4 @@ lonp, latp, zp, t, T0, U, V = tracpy.run.run(tp, date, lon0, lat0)
 # plot the tracks
 tracpy.plotting.tracks(lonp, latp, tp.name, tp.grid)
 
-#tracpy.plotting.hist(lonp, latp, tp.name, grid=tp.grid, which='pcolor', bins=(50,50))
+#tracpy.plotting.hist(lonp, latp, tp.name, grid=tp.grid, which='hist2d', bins=(50,50))
